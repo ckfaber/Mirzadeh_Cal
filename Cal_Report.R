ff8f82a902e2df5e841583658dca492c3c847c3a
Chelsea Faber <kasper.chelsea@gmail.com>
2022-09-29 15:47:59 -0700

    Moves gt tables from Cal_Plot to new Cal_Report script

---
+++ Cal_Report.R
@@ -0,0 +1,78 @@
+## Summary parameters ----------------------------------------------------------
+
+# Remind yourself of the variable names
+colnames(df)
+
+# Select which ones you'd like to see summary statistics for
+cols_to_eval <- c("VO2","RER","EE")
+
+# How do you want to group the data?
+#groups_for_eval <- c("Sex","Group","Treatment")
+
+# Quick experimental overview
+df %>%
+  summarize(
+    across(Sex:Animal,n_distinct)
+  ) %>%
+  gt() %>%
+  tab_header(
+    title = md(glue("Summary of cohort **{cohort}** calorimetry study beginning {exp_date}")),
+    subtitle = md("*Counts* per column")
+  ) %>%
+  opt_align_table_header(align = "left")
+
+# Summary statistics by group for variables selected above
+
+# Mean
+df %>%
+  group_by(Treatment) %>%
+  summarize(
+    across(all_of(cols_to_eval),mean) # this is where gt breaks currently - can only put one function as input to across
+  ) %>%
+  gt(rowname_col = "Treatment") %>%
+  tab_header(
+    title = md(glue("Column mean of **{cohort}** grouped by *Treatment*")),
+    subtitle = md(glue("From calorimetry study beginning {exp_date}"))
+  ) %>%
+  opt_align_table_header(align = "left")
+
+# Median
+
+# Min
+df %>%
+  group_by(Treatment) %>%
+  summarize(
+    across(cols_to_eval,min) # this is where gt breaks currently - can only put one function as input to across
+  ) %>%
+  gt(rowname_col = "Treatment") %>%
+  tab_header(
+    title = md(glue("Column min values of **{cohort}** grouped by *Treatment*")),
+    subtitle = md(glue("From calorimetry study beginning {exp_date}"))
+  ) %>%
+  opt_align_table_header(align = "left")
+
+# Max
+df %>%
+  group_by(Treatment) %>%
+  summarize(
+    across(cols_to_eval,max) # this is where gt breaks currently - can only put one function as input to across
+  ) %>%
+  gt(rowname_col = "Treatment") %>%
+  tab_header(
+    title = md(glue("Column max values of **{cohort}** grouped by *Treatment*")),
+    subtitle = md(glue("From calorimetry study beginning {exp_date}"))
+  ) %>%
+  opt_align_table_header(align = "left")
+
+# SD
+df %>%
+  group_by(Treatment) %>%
+  summarize(
+    across(cols_to_eval,sd) # this is where gt breaks currently - can only put one function as input to across
+  ) %>%
+  gt(rowname_col = "Treatment") %>%
+  tab_header(
+    title = md(glue("Column sd values of **{cohort}** grouped by *Treatment*")),
+    subtitle = md(glue("From calorimetry study beginning {exp_date}"))
+  ) %>%
+  opt_align_table_header(align = "left")
