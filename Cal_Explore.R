396ce5a79aafea155e5a2cfb65bd4ebfb3b883c8
Chelsea Faber <kasper.chelsea@gmail.com>
2023-12-14 12:47:08 -0700

    Converts Cal_Explore to .qmd

--- Cal_Explore.R
+++
@@ -1,8 +0,0 @@
-p <- ggplot(df.hourly,
-                 mapping = aes(x = Time, 
-                               y = AllMeters,
-                               color = .data[[groupvar]],
-                               group = Animal,
-                               fill = .data[[groupvar]]))
-
-p + geom_line(mapping = aes(linetype = Animal),stat = "identity")
